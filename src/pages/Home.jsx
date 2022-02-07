95b90ef9d2c2dd6e1cc6d76611ce9de8c67ffb3b
Zahid <jahidulhaque767@gmail.com>
2022-02-08 04:31:43 +0600

    modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Home/Banner/Banner";
 import FeaturedProducts from "../components/Home/FeaturedProducts/FeaturedProducts";
+import LatestProduct from "../components/Home/LatestProduct/LatestProduct";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <FeaturedProducts />
+      <LatestProduct />
     </div>
   );
 };
