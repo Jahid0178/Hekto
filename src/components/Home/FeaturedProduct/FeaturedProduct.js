2f5d2faaa836c89be55c59c33e5edf30539777ce
Zahid <jahidulhaque767@gmail.com>
2022-02-08 03:24:49 +0600

    added featured product section

---
+++ src/components/Home/FeaturedProduct/FeaturedProduct.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "../../Styles/Container.styles";
+import "./FeaturedProduct.css";
+
+const FeaturedProduct = () => {
+  return (
+    <div>
+      <Container>
+        <h1>Hi</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default FeaturedProduct;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Home/Banner/Banner";
+import FeaturedProduct from "../components/Home/FeaturedProduct/FeaturedProduct";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <FeaturedProduct />
     </div>
   );
 };
