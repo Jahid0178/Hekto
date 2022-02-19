8d53bcef10da7e69df7e8d86bcdf40b84a020d99
Zahid <jahidulhaque767@gmail.com>
2022-02-20 04:36:00 +0600

    added firebase init and config

---
+++ src/components/LogIn/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdZEp7oxgxTGNcHYpec7Jp-FUI6JSGC_Q",
+  authDomain: "hekto-f4a48.firebaseapp.com",
+  projectId: "hekto-f4a48",
+  storageBucket: "hekto-f4a48.appspot.com",
+  messagingSenderId: "750351778672",
+  appId: "1:750351778672:web:5d8e9e46a3b25341d045b5",
+};
+export default firebaseConfig;
